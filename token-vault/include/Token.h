3b029675b0642b15a0d04f0e79bf714fa99e8b9a
NioZow <niozow@proton.me>
2025-02-22 20:05:05 +0100

    fix build issues

--- token-vault/include/Token.h
+++ token-vault/include/Token.h
@@ -791,20 +791,6 @@ DECLSPEC_IMPORT BOOL ADVAPI32$CreateWellKnownSid(
 	IN OUT DWORD *         cbSid
 );
 
-DECLSPEC_IMPORT NTSTATUS NTDLL$RtlAllocateAndInitializeSid(
-	PSID_IDENTIFIER_AUTHORITY IdentifierAuthority,
-	UCHAR                     SubAuthorityCount,
-	ULONG                     SubAuthority0,
-	ULONG                     SubAuthority1,
-	ULONG                     SubAuthority2,
-	ULONG                     SubAuthority3,
-	ULONG                     SubAuthority4,
-	ULONG                     SubAuthority5,
-	ULONG                     SubAuthority6,
-	ULONG                     SubAuthority7,
-	PSID *                    Sid
-);
-
 DECLSPEC_IMPORT BOOL ADVAPI32$LookupAccountNameW(
 	IN OPTIONAL LPCWSTR lpSystemName,
 	IN LPCWSTR          lpAccountName,
